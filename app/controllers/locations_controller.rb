0ded49a62df73415f7a2a150d2034645ab9c18f4
Tobias Andre Eggers <tobiasae@gmail.com>
2020-01-17 08:56:21 -0300

    creating adapter, service and controller to return coordinates from a location name

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class LocationsController < ApplicationController
+  def search_coordinates
+    return params_missing? if empty_params?
+
+    coordinates = Rails.cache.fetch(cache_key, expires_in: 30.days) do
+      LocationService.new(location_params).perform
+    end
+
+    render json: coordinates.to_json
+  rescue LocationAdapter::LocationUnavailable
+    render json: { error: "Location not found", value: 0 }, status: :unprocessable_entity
+  end
+
+  private
+
+  def empty_params?
+    params[:location].blank?
+  end
+
+  def params_missing?
+    render json: { "message": "all parameters are required" }, status: :unprocessable_entity
+  end
+
+  def location_params
+    {
+      string_location: params.dig(:location)
+    }
+  end
+
+  def cache_key
+    "#{params.dig(:location)}}"
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,10 +13,13 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.active_record.cache_versioning = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp", "caching-dev.txt").exist?
+    config.action_controller.perform_caching = true
+
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
@@ -24,27 +27,17 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
+    config.cache_store = :redis_store, {
+      expires_in: 90.days,
+      namespace: "cache",
+      redis: { host: "redis", port: 6379, db: 0 }
+    }
   end
-
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
-
-  # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
-
-  # Raises error for missing translations.
+  # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
